4fcfc180db1142552bd169f033a1a5633e05909e
Ellen Bennett <bennette831@gmail.com>
2023-09-20 23:27:29 -0600

    add attributes to CashRegister, create add_item method

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0, total=0, items=[]):
+        self.discount = discount
+        self.total = total
+        self.items = items
+
+    def add_item(self, title, price):
+        pass
